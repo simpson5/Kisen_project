3ef3db71b8c37d139252befe3723789094d2722d
YujinJeon416 <75886211+YujinJeon416@users.noreply.github.com>
2021-06-15 21:02:12 +0900

    전유진 attachment,ReviewExt,controller,dao,service추가

--- kisen/src/main/java/com/simpson/kisen/review/model/dao/ReviewDaoImpl.java
+++ kisen/src/main/java/com/simpson/kisen/review/model/dao/ReviewDaoImpl.java
@@ -8,7 +8,9 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.simpson.kisen.review.model.vo.Attachment;
 import com.simpson.kisen.review.model.vo.Review;
+import com.simpson.kisen.review.model.vo.ReviewExt;
 
 @Repository
 public class ReviewDaoImpl implements ReviewDao {
@@ -35,6 +37,41 @@ public class ReviewDaoImpl implements ReviewDao {
 	public int selectReviewTotalContents() {
 		return session.selectOne("review.selectReviewTotalContents");
 		
+	}
+	
+	@Override
+	public int insertReview(ReviewExt review) {
+		return session.insert("review.insertReview", review);
+	}
+
+	@Override
+	public int insertAttachment(Attachment attach) {
+		return session.insert("review.insertAttachment", attach);
+	}
+
+	@Override
+	public ReviewExt selectOneReview(int no) {
+		return session.selectOne("review.selectOneReview", no);
+	}
+
+	@Override
+	public List<Attachment> selectAttachList(int review_No) {
+		return session.selectList("review.selectAttachList", review_No);
+	}
+
+	@Override
+	public ReviewExt selectOneReviewCollection(int no) {
+		return session.selectOne("review.selectOneReviewCollection", no);
+	}
+
+	@Override
+	public Attachment selectOneAttachment(int no) {
+		return session.selectOne("review.selectOneAttachment", no);
+	}
+
+	@Override
+	public List<Review> searchTitle(String searchTitle) {
+		return session.selectList("review.searchTitle", searchTitle);
 	}
 
 	
